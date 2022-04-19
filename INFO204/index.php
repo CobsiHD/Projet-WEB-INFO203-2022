e634e090c31abd9ab2166889fad83ed30e389f6d
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-04-19 09:39:32 +0200

    2e jet

--- INFO204/index.php
+++ INFO204/index.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="utf-8">
     <link href="css/styles.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <title>Accueil</title>
 </head>
 
@@ -20,32 +21,32 @@
 
 <section class="container">
     <div id="cube">
-        <div class="front">
+        <div class="front" id="1">
             <span class="dot dot1"></span>
         </div>
-        <div class="back">
+        <div class="back" id="2">
             <span class="dot dot1"></span>
             <span class="dot dot2"></span>
         </div>
-        <div class="right">
+        <div class="right" id="3">
             <span class="dot dot1"></span>
             <span class="dot dot2"></span>
             <span class="dot dot3"></span>
         </div>
-        <div class="left">
+        <div class="left" id="4">
             <span class="dot dot1"></span>
             <span class="dot dot2"></span>
             <span class="dot dot3"></span>
             <span class="dot dot4"></span>
         </div>
         <div class="top">
-            <span class="dot dot1"></span>
+            <span class="dot dot1" id="5"></span>
             <span class="dot dot2"></span>
             <span class="dot dot3"></span>
             <span class="dot dot4"></span>
             <span class="dot dot5"></span>
         </div>
-        <div class="bottom">
+        <div class="bottom" id="6">
             <span class="dot dot1"></span>
             <span class="dot dot2"></span>
             <span class="dot dot3"></span>
@@ -64,12 +65,17 @@
             var xRand = getRandom(max, min);
             var yRand = getRandom(max, min);
 
+            console.log(max, min);
+            console.log(xRand, yRand)
+
             cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
         }
 
         function getRandom(max, min) {
             return (Math.floor(Math.random() * (max-min)) + min) * 90;
+
         }
+
     </script>
 
 </section>
