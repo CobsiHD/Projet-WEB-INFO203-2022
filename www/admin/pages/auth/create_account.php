9b68401d21ce52ab38006005bbd846936b4cc119
nocxxy <evan.meziere@gmail.com>
2022-04-29 11:16:10 +0200

    auth
    création des pages login et create account

---
+++ www/admin/pages/auth/create_account.php
@@ -0,0 +1,34 @@
+<?php
+include("../../db/db_connect.php");
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Create account </title>
+	    <link rel="stylesheet" href="../../css/styles_auth.css">
+    </head>
+    <body>
+        <form method="post">
+            Login                 : <input type="text" name="login">
+            Password              : <input type="password" name="passwd">
+            Confirmation Password : <input type="password" name="conf_passwd">
+            <input type="submit" value="Création de Compte">
+        </form>
+        <?php
+        include("../../lib/crud/authentification.crud.php");
+        if (isset($_POST["login"]) && isset($_POST["passwd"]) && isset($_POST["conf_passwd"])){
+            $log = $_POST["login"];
+            $pwd = $_POST["passwd"];
+            $conf_pwd = $_POST["conf_passwd"];
+            if ($pwd == $conf_pwd) {
+                $pwd=md5($pwd);
+                create_auth($conn,$log,$pwd);
+                echo("Le compte a bien été créer !");
+            };
+        };
+        ?>
+    </body>
+</html>
+<?php 
+include("../../db/db_connect.php");
+?>
---
+++ www/admin/pages/auth/login.php
@@ -0,0 +1,39 @@
+<?php
+include("../../db/db_connect.php");
+session_start() ;
+if(isset($_POST["login"]) && isset($_POST["passwd"])){
+    $log=$_POST["login"];
+    $ret= select_auth($conn,$log);
+    if( isset($ret["pwd"])){
+		$pwd=$ret["pwd"];
+	};
+    if(md5($_POST["passwd"])==$pwd){
+		/* session admin */
+		$_SESSION["start"]=time() ; 
+	
+		/* redirection */
+		header("Location:../../index.php") ;
+    }
+	else {
+		header("Location:login.php") ; 
+	}
+}
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title> Login </title>
+	<link rel="stylesheet" href="../../css/styles_auth.css">
+</head>
+<body>
+    <form method="POST" action="login_form.php">
+    Login    :	<input type="text" name="login">
+    Password :  <input type="text" name="passwd">
+        <input type="submit" value="Connection">
+    </form>
+    <a href="create_auth.php">Créer Un Compte</a>
+</body>
+<?php
+include("../../db/db_disconnect.php");
+?>
+</html>
