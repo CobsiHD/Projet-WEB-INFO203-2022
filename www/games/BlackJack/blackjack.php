1f1328e884c7ccaf18b36a04c16f22356986c113
nocxxy <evan.meziere@gmail.com>
2022-04-29 19:26:29 +0200

    BlackJack
    Debut du BlackJack

---
+++ www/games/BlackJack/blackjack.php
@@ -0,0 +1,41 @@
+<?php
+include("../../db/db_connect.php");
+session_start();
+if (!isset($_SESSION["start"]) or $_SESSION["start"]==0.0){
+	header("Location:../../admin/pages/auth/login.php");
+} else if (isset($_SESSION["start"])){
+	header("Location:blackjack.php")
+}
+?>
+
+
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title> BlackJack </title>
+	<link rel="stylesheet" href="../../css/styles_blackjack.css">
+</head>
+<body>
+<header>
+</header>
+<section>
+	<h2> Dealer : <span id="dealer-sum"></span></h2>
+	<div id="dealer-cards">
+		<img id="hide" src="../../images/cartes/BACK.png">
+	</div>"
+
+	<h2>Vos Cartes : <span id="player-sum"></span> </h2>
+	<div id="player-cards"></div>
+	<br>
+	<button id="hit">Prendre une Carte </button>
+	<button id="stay">Garder votre Main </button>
+	<p id="result"></p>
+</section>
+<footer>
+</footer>
+
+</body>
+</html>
+<?php
+include("../../db/db_disconnect.php");
+?>
---
+++ www/lib/js/script_blackjack.js
@@ -0,0 +1,32 @@
+var dealerscore = 0;
+var playerscore = 0; //Variable pour compter le nombre de points de chaque joueur//
+
+var dealeracecount = 0;
+var playeracecount = 0; // Variable qui permete de compter le nombre d'as//
+
+var hide;
+var deck;
+
+var pioche = true ; // Variable qui dit si le joueur peut piocher tant que le score du jouer est <=21//
+
+window.onload = function(){
+    constrution_deck();
+    melange_deck();
+}
+function constrution_deck(){
+    // Fonction sans paramêtre qui crée un jeu de carte avec cahque carte le nom de la carte d'un jeu de 52 cartes//
+    let valeur = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let types = ["C","D","H","S"]; // Initial des Types de cartes en anglais pour éviter toute confusions entre le coeur et le carreau//
+    deck = [];
+    for (let i =0; i < types.length ; i++) {
+        for (let j=0 ; j < valeur.length ; j++ ){
+            deck.push(valeur[j]) + "-" + types[i]);// On crée 2 boucle qui parcourt les 2 tableaux pour créer à chaque fois la carte de différent type et valeur.//
+        };
+    };
+    console.log(deck);
+}
+function melange_deck() {
+    for (let i = 0; i< deck.length ; i++){
+
+    }
+}
