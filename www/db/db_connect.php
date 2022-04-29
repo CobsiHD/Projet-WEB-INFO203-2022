f6c8c8f5eb9acd9ed88b68b5f1bcfb0a6160a49c
nocxxy <evan.meziere@gmail.com>
2022-04-29 10:23:24 +0200

    Db
    création des fonction connect et disconect pour la base de donnée

---
+++ www/db/db_connect.php
@@ -0,0 +1,4 @@
+<?php
+$conn=mysqli_connect("localhost","l1_info_5","FY45","l1_info_5");
+mysqli_set_charset($conn,"utf8");
+?>
---
+++ www/db/db_disconnect.php
@@ -0,0 +1,4 @@
+<?php
+#4− fermeture de la connexion
+mysqli_close($conn);
+?>
