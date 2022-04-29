4550cef547cee6c42622260aa09ae79d258790ac
nocxxy <evan.meziere@gmail.com>
2022-04-29 09:50:19 +0200

    accueil
    debut css + debut de l'accueil

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Top 5</title>
+  <link rel="stylesheet" href="./css/style_accueil.css">
+</head>
+<body>
+    <header>
+        <img href="./images/logo.png">
+        <img href="./images/menu.png">
+    </header>
+    <p> Hello world</p>
+    <footer>
+        <p>Conctacté nous avec nos réseaux
+            <a href=""><img href="./images/logo_twiter.png"></a>
+            <a href=""><img href="./images/logo_insta.png"></a>
+            <a href=""><img href="./images/logo_facebook.png"></a>
+            <a href=""><img href="./images/logo_github.png"></a>
+            <a href=""><img href="./images/logo_ph.png"></a>
+        </p>
+    </footer>
+</body>
+</html>
