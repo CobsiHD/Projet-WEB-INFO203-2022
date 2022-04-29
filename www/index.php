89e828730c6c2a704fe942321e37f66d9e6a6f28
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-04-29 10:17:46 +0200

    creation dossier image

--- www/index.php
+++ www/index.php
@@ -7,12 +7,17 @@
   <link rel="stylesheet" href="./css/styles_accueil.css">
 </head>
 <body>
+
     <header>
         <img href="./images/logo.png">
         <img href="./images/menu.png">
     </header>
+
+
     <p> Hello world</p>
     <footer>
+
+
         <p>Conctacté nous avec nos réseaux
             <a href=""><img href="./images/logo_twiter.png"></a>
             <a href=""><img href="./images/logo_insta.png"></a>
