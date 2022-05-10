336e1bd33baec3b3b11ebd1323dd6924c4e35b68
Jules <102732657+JulesChe@users.noreply.github.com>
2022-05-10 16:05:33 +0200

    petit test pas de changement maj

--- www/lib/js/script_pages_jeux.js
+++ www/lib/js/script_pages_jeux.js
@@ -13,7 +13,6 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("custom-slider");
     var dots = document.getElementsByClassName("dot");
-    slides[slideIndex].style.display = "none";
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -22,6 +21,6 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
