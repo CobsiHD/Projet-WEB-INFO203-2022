92885103c73ef719ffab38aa758c563bed9499e8
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-05-11 20:18:36 +0200

    Accueil
    Creaiton du header + footer
    
    puis installation de bootstrap et jquery pour les animations / transition / detection
    
    creation des css

--- www/index.php
+++ www/index.php
@@ -5,64 +5,111 @@
   <meta charset="utf-8">
   <title>Accueil</title>
   <link rel="stylesheet" href="./css/styles_accueil.css">
+
 </head>
 <body>
 
+<header>
+    <h1>Bienvenue au CASINO !</h1>
+    <nav>
+        <a href="">Jouer</a>
+        <a href="">Info Jeux</a>
+        <a href="">Classement</a>
+        <a href="">Connexion</a>
+    </nav>
+</header>
+<div class="main">
+    <section id='steezy'>
+        <h2> Lorem ipsum dolor sit amet</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex libero cupiditate deserunt earum, fuga, ab labore esse animi, nihil est tenetur non delectus perferendis. Maxime accusamus ea rem sint at!</p>
 
-    <header class="header sticky sticky--top js-header">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ut aspernatur omnis neque iusto. Veritatis rem ad atque optio perferendis. Dolorum qui consequatur, repellat soluta impedit porro totam molestiae sit.</p>
 
-        <div class="grid">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus iusto, omnis quas, rem minus cupiditate, sunt voluptates minima sint nemo ipsa itaque consequatur dolor, quam necessitatibus vitae. Quod, eaque, quam.</p>
 
-            <nav class="navigation">
-                <ul class="navigation__list navigation__list--inline">
-                    <li class="navigation__item"><a href="#" class="navigation__link navigation__link--is-active">Home</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link"> Jouer </a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link">Info Jeux</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link">Classement Général</a></li>
-                    <li class="navigation__item connexion"><a href="#" class="navigation__link">Se connecter</a></li>
-                </ul>
-            </nav>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
 
-        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+    </section>
 
-    </header>
+    <section id='real'>
+        <h2> Lorem ipsum dolor sit amet</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+    </section>
+
+    <section id='big'>
+        <h2>Lorem ipsum dolor sit amet</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim sunt delectus, reiciendis omnis laudantium animi, et nulla quidem, aliquam aliquid. A consectetur deleniti voluptates eius neque, voluptate voluptatibus nostrum!</p>
+
+    </section>
+</div>
+
 
     <main class="main">
 
-        <div class="grid">
-
-            <h1>Lorem ipsum dolor sit.</h1>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id sed voluptatem error vel eius placeat harum voluptate doloribus ratione in, vero aliquid et, itaque, repellat magnam nisi adipisci unde, odit esse architecto hic! Eaque libero officia laudantium laboriosam non velit vero pariatur, rem nisi commodi dolores, culpa quibusdam dolorem nemo voluptatem. Ipsam, molestias autem iste est quia magni iusto distinctio esse magnam possimus tenetur dolor, suscipit fugiat ad? Natus officia magnam ipsam. Nisi aut quis natus voluptatum, sapiente inventore quidem similique odio, quod iste delectus commodi maxime asperiores doloribus officiis fuga, eaque, beatae ipsam assumenda architecto. At minima pariatur qui!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem numquam sint nisi natus fugiat quibusdam, iusto unde tenetur consequuntur, dolorum laborum consectetur explicabo at blanditiis aperiam perferendis totam neque vitae eos quisquam, ratione cum id fuga? Ad harum dolores optio debitis, eaque excepturi, iste totam. Quasi, maiores eligendi laudantium nobis harum ullam nihil voluptas officia id molestiae animi deleniti placeat dolorum atque fuga. Mollitia ducimus corporis natus doloribus blanditiis, voluptatem officia nesciunt sit dolorem incidunt illum vel voluptas, exercitationem nemo repellendus deleniti! Vero rerum natus blanditiis voluptatibus nihil deserunt, vitae nulla reprehenderit perferendis praesentium minus, dolor consequuntur repellat repudiandae, facilis veritatis dolorem? Nisi magni dolor, ut maiores voluptas rem amet commodi est distinctio perferendis mollitia dicta dolores, officiis dignissimos dolorem ea soluta ex quae nemo. Cupiditate ab ratione voluptatem non impedit, tempora voluptas ipsam iste sed a, animi sunt veniam deserunt molestias nobis excepturi. Earum inventore vitae recusandae, sint. Aut.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo magni maxime rerum minus impedit autem explicabo ex eius perspiciatis libero ipsam ab harum maiores rem aliquid inventore, dolor optio quas, temporibus, deleniti soluta expedita similique. Aperiam ad quasi sed officia dolorem, consectetur ipsum, vel possimus fugiat expedita adipisci, porro ea inventore accusantium, blanditiis nam minus. Laudantium similique amet et odio culpa quibusdam vel doloremque voluptatem totam a sit blanditiis placeat, beatae quasi esse dicta, earum alias delectus hic libero magni ipsam eveniet tempore. Inventore vitae error, provident ipsum quidem atque, tempora laboriosam at totam unde illo eligendi magnam reiciendis nam magni ut, laudantium deserunt odio voluptatibus maiores facilis. Voluptas ea consectetur, totam non possimus laudantium at quibusdam nihil doloribus, porro ipsum qui animi itaque velit.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla adipisci a vero sint ducimus provident quas, eveniet tenetur, corporis ex quidem aliquam consequatur quam dicta dolores modi nisi dolorem magnam, consequuntur quasi assumenda saepe similique eos accusantium, odio! Excepturi illo corporis, magni eaque animi esse, odit minima expedita facilis repellendus, culpa magnam eius delectus consequatur tenetur. Totam magnam voluptate mollitia quod omnis, hic, labore, veniam molestiae amet, quam distinctio optio nobis tenetur similique blanditiis. Fugiat obcaecati repudiandae culpa totam quasi perspiciatis rerum quibusdam architecto asperiores, enim quisquam error numquam officiis, voluptatibus debitis vitae sint. Neque ipsam, nobis sunt ut consectetur. Doloremque velit quasi necessitatibus inventore earum rem enim omnis aut doloribus laboriosam accusamus consequatur libero facere vitae dolorem mollitia voluptates molestias fugiat laborum dignissimos esse, nesciunt totam, blanditiis accusantium! Provident perspiciatis deserunt nostrum. Et provident optio enim perspiciatis tenetur distinctio officia, molestias in repellendus eius quis tempora animi. Reprehenderit, veniam.</p>
-
+        <div class="slide">
+            <a bonjour></a>
         </div>
 
     </main>
 
 
+
+
     <footer>
-
-
-        <p>Conctacté nous avec nos réseaux
-            <a href=""><img href="./images/logo_twiter.png"></a>
-            <a href=""><img href="./images/logo_insta.png"></a>
-            <a href=""><img href="./images/logo_facebook.png"></a>
-            <a href=""><img href="./images/logo_github.png"></a>
-            <a href=""><img href="./images/logo_ph.png"></a>
-        </p>
+        <div class="footer">
+            <footer>
+                <div class="social">
+                    <a href="#"><i class="icon icon-social-instagram"></i>
+                    </a><a href="#"><i class="icon icon-social-snapchat"></i>
+                    </a><a href="#"><i class="icon icon-social-twitter"></i></a>
+                    <a href="#"><i class="icon icon-social-facebook"></i></a>
+                </div>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#">Accueil</a></li>
+                    <li class="list-inline-item"><a href="#">Jeux</a></li>
+                    <li class="list-inline-item"><a href="#">A propos</a></li>
+                    <li class="list-inline-item"><a href="#">Contact</a></li>
+                </ul>
+                <p class="copyright">TeamZboub © 2022</p>
+            </footer>
+        </div>
     </footer>
+
+<!-- content -->
+<script src="lib/js/jquery.js"></script>
+<script src="lib/js/bootstrap.js"></script>
+
 </body>
 </html>
 
 <script>
-
-    window.onscroll = function scroll() {
-        console.log("yolo");
-    }
-
-
-
+    /* Script Pour scroll header */
+    $(function() {
+        $(window).scroll(function () {
+            var winTop = $(window).scrollTop();
+            if (winTop >= 30) {
+                $("body").addClass("sticky-header");
+            } else {
+                $("body").removeClass("sticky-header");
+            }
+        });
+    });
 </script>
