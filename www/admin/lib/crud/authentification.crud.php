ce2642949d7856e4723944a157f3f325bdd1ca1a
Jules <102732657+JulesChe@users.noreply.github.com>
2022-05-19 23:15:56 +0200

    Update authentification.crud.php

--- www/admin/lib/crud/authentification.crud.php
+++ www/admin/lib/crud/authentification.crud.php
@@ -1,18 +1,18 @@
 <?php
 function create_auth($conn,$log,$pwd){
-	$sql="INSERT INTO `authentification`(`login`, `password`) values ('$log', '$pwd')";
+	$sql="INSERT INTO `authentification`(`login`, `password`) VALUES ('$log','$pwd')";
 	$ret=mysqli_query($conn, $sql) ;
 	return $ret ;
 }
 function select_auth($conn,$log){
-	$sql="SELECT * FROM `authentification` WHERE `login`='$log'";
+	$sql="SELECT `login`, `password` FROM `authentification` WHERE `login`='$log'";
 	if($ret=mysqli_query($conn, $sql)){
 		$ret=mysqli_fetch_assoc($ret);
 	}
 	return $ret ;
 };
 function update_auth($conn, $log, $pwd){
-	$sql="UPDATE `authentification` set `pwd`='$pwd' WHERE `login`='$log'" ;
+	$sql="UPDATE `authentification` SET `password`='$pwd' WHERE `login`='$log'" ;
 	$ret=mysqli_query($conn, $sql) ;
         return $ret ; 
 }
