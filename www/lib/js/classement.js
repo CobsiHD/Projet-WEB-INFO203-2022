a7382f3a23e546338cecf04d5795e0ec20ad87cf
Jules <102732657+JulesChe@users.noreply.github.com>
2022-05-19 16:32:24 +0200

    classement

--- www/lib/js/classement.js
+++ www/lib/js/classement.js
@@ -4,19 +4,23 @@ window.onload = function(){
     showGen();
 
 }
-function showGen(){
+function showGen() {
+
+    var table = document.getElementsByTagName("table");
+    var i;
+
     for (i = 0; i < 7; i++) {
 
-        if (table[i].id !== "general" && table[i].style.display !== "none"){
+        if (table[i].id !== "general" && table[i].style.display !== "none") {
             table[i].style.display = "none";
-        }
-        else if(table[i].id === "general" && table[i].style.display === "none"){
+        } else if (table[i].id === "general" && table[i].style.display === "none") {
 
             table[i].style.display = "table";
         }
 
     }
 
+}
 function show(event) {
 
     var evenement = event.currentTarget.id;
@@ -28,6 +32,7 @@ function show(event) {
 
         if (table[i].id !== evenement && table[i].style.display !== "none") {
             table[i].style.display = "none";
+
         } else if (table[i].id === evenement && table[i].style.display === "none") {
 
             table[i].style.display = "table";
@@ -35,4 +40,4 @@ function show(event) {
 
     }
 
-}}
+}
