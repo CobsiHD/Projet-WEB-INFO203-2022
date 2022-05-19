beb9ec3c78f277f910487bb1bf48caa928676122
Jules <102732657+JulesChe@users.noreply.github.com>
2022-05-19 23:22:52 +0200

    update crud classement

--- www/admin/lib/crud/classements.crud.php
+++ www/admin/lib/crud/classements.crud.php
@@ -12,7 +12,7 @@ function select_class($conn,$user){
 	return $ret ;
 };
 function update_class($conn, $user,$general,$des,$bonneteau,$roulette,$blackjack,$machine_a_sous,$chevaux){
-	$sql="UPDATE `classements` SET `user`=$user,`general`=$general`des`=$des,`bonneteau`=$bonneteau,`roulette`=$roulette,`blackjack`=$blackjack,`machine_a_sous`=$machine_a_sous,`chevaux`=$chevaux WHERE `user`=$user" ;
+	$sql="UPDATE `classements` SET `user`='$user',`general`=$general,`des`=$des,`bonneteau`=$bonneteau,`roulette`=$roulette,`blackjack`=$blackjack,`machine_a_sous`=$machine_a_sous,`chevaux`=$chevaux WHERE `user`='$user'" ;
 	$ret=mysqli_query($conn, $sql) ;
         return $ret ; 
 }
