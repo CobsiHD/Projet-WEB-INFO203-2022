3e93dfc85b6a246e99f45efafeae43a7db825d2b
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-05-19 22:43:43 +0200

    css index

--- www/index.php
+++ www/index.php
@@ -1,3 +1,9 @@
+<?php session_start();
+
+include("./db/db_connect.php");
+?>
+
+
 <!doctype html>
 <html lang="fr">
 
@@ -7,6 +13,7 @@
   <link rel="stylesheet" href="./css/styles_accueil.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
 
 </head>
@@ -106,6 +113,8 @@
 
 </body>
 </html>
+
+<?php include("./db/db_disconnect.php"); ?>
 
 
 
