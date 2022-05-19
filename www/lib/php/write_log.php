5c7ccbea87dedbd094abdc4c9163f7d0d4bf7462
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-05-19 09:14:42 +0200

    update index + dé

--- www/lib/php/write_log.php
+++ www/lib/php/write_log.php
@@ -3,26 +3,24 @@ include("../../admin/lib/crud/classements.crud");
 if(isset($_SESSION["log"]) && isset($_SESSION["user"])){
     if(!$_SESSION["log"]){
         echo("<ul class='navbar-nav ml-auto'>
-        <li class='nav-item'><a href='admin/pages/auth/login.php' class='nav-link rp-4'>Se connecter</a></li>
-                    <li class='nav-item'><a href='admin/pages/auth/create_account.php' class='nav-link rp-4'>s'inscrire</a></li>
-    </ul>")
+        <li class='nav-item'><a href='../../admin/pages/auth/login.php' class='nav-link rp-4'>Se connecter</a></li>
+                    <li class='nav-item'><a href='../../admin/pages/auth/create_account.php' class='nav-link rp-4'>s'inscrire</a></li>
+    </ul>");
     } else {
         echo("<ul> class='navbar-nav ml-auto'>");
-        echo("<li class='nav-item'><img src='./images/img_profil.jpeg'></li>");
+        echo("<li class='nav-item'><img src='../../images/img_profil.jpeg'></li>");
         echo("<li class='nav-item'> <ul><li>User :");
         echo($_SESSION["user"]);
         echo("</li><li> Votre Monnaie :");
         $info_user=select_class($conn,$_SESSION["user"]);
         echo($info_user["general"]);
         echo("</li><li>");
-        echo("<a href='./pages/info_account'>Cliquez-ici pour plus d'info</a></li><li><a href='disconnect.php'>Deconnexion</a></li></ul></li></ul>");
+        echo("<a href='../../pages/info_account.php'>Cliquez-ici pour plus d'info</a></li><li><a href=''>Deconnexion</a></li></ul></li></ul>");
     }
 } else {
     echo("<ul class='navbar-nav ml-auto'>
-        <li class='nav-item'><a href='admin/pages/auth/login.php' class='nav-link rp-4'>Se connecter</a></li>
-                    <li class='nav-item'><a href='admin/pages/auth/create_account.php' class='nav-link rp-4'>s'inscrire</a></li>
-    </ul>")
+        <li class='nav-item'><a href='../../admin/pages/auth/login.php' class='nav-link rp-4'>Se connecter</a></li>
+                    <li class='nav-item'><a href='../../admin/pages/auth/create_account.php' class='nav-link rp-4'>s'inscrire</a></li>
+    </ul>");
 }
-
-
 ?>
--- www/pages/Dice/index.php
+++ www/pages/Dice/index.php
@@ -18,7 +18,6 @@
 
 </header>
 
-
 <section class="container">
     <div id="cube">
         <div class="front" id="1">
@@ -62,30 +61,46 @@
         var cube = document.getElementById('cube');
 
         var min = 1;
-        var max = 30;
+        var max = 6;
 
         cube.onclick = function() {
-            var xRand = getRandom(max, min);
-            var yRand = getRandom(max, min);
+            var rand = getRandom(max, min);
 
+            if(rand === 6){
+               var yrand = 0;
+               var xrand = -1620;
+            } else if( rand === 5){
+                var yrand = 0;
+                var xrand = -1530;
+            }else if( rand === 4){
+                var yrand = 1530;
+                var xrand = 0;
+            }else if( rand === 3){
+                var yrand = -1530;
+                var xrand = 0;
+            }else if( rand === 2){
+                var yrand = 0;
+                var xrand = 1530;
+            }else if( rand === 1){
+                var yrand = 1440;
+                var xrand = 1440;
+            }
 
+            let yrot = 1800 +yrand;
+            let xrot = 1800 +xrand;
 
             console.log(max, min);
-            console.log(xRand, yRand)
+            console.log(rand)
 
-            cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+            cube.style.transform = 'rotateX('+xrot+'deg) rotateY('+yrot+'deg)';
         }
 
         function getRandom(max, min) {
-            return (Math.floor(Math.random() * (max-min)) + min) * 90;
+            return (Math.floor(Math.random() * (max-min)) + min);
 
         }
 
-
-
-
     </script>
-
 </section>
 
 <footer>
