4bd6469e23f0ef72c4353fa34ca75f9400b2b380
nocxxy <evan.meziere@gmail.com>
2022-05-19 23:47:09 +0200

    Update info account

--- www/lib/php/fct_win.php
+++ www/lib/php/fct_win.php
@@ -8,24 +8,19 @@ function win($mise,$jeux,$win,$user){
 Entré $jeux en str pour dire quel jeux à gagné,$mise en int pour dire la somme qui a été parié, $win en booléen et enfin $user le joueur qui a gagné*/
 global $conn ;
 $info_joueur= select_class($conn,$user);
-echo($info_joueur);
 
 if ($win){
-echo($info_joueur[$jeux]);
 
 $monnaie_jeux = $info_joueur[$jeux] + $mise ; //rajoute la mise au jeux en question du joueur
 $info_joueur[$jeux] = $monnaie_jeux ;
-echo($info_joueur[$jeux]);
 
 $monnaie_general=$info_joueur['general'] + $mise ; // rajoute la mise au géneral
 
 update_class($conn, $user,$monnaie_general,$info_joueur['des'],$info_joueur['bonneteau'],$info_joueur['roulette'],$info_joueur['blackjack'],$info_joueur['machine_a_sous'],$info_joueur['chevaux']); //modifie les donnée de la basse de donnée
 
 }else {
-    echo($info_joueur[$jeux]);
     $monnaie_jeux = $info_joueur[$jeux] - $mise ;
     $info_joueur[$jeux] = $monnaie_jeux ;
-    echo($info_joueur[$jeux]);
     $monnaie_general=$info_joueur['general'] - $mise ;
 
     update_class($conn, $user,$monnaie_general,$info_joueur['des'],$info_joueur['bonneteau'],$info_joueur['roulette'],$info_joueur['blackjack'],$info_joueur['machine_a_sous'],$info_joueur['chevaux']);
--- www/pages/info_account.php
+++ www/pages/info_account.php
@@ -10,6 +10,7 @@ session_start() ;
         <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     </head>
     <body>
+    <section class = 'auth'>
     <div class="box">
             <div class="container">
                 <div class="form">
@@ -17,19 +18,21 @@ session_start() ;
                 <?php 
                 include("../admin/lib/crud/classements.crud.php");
                 $info=select_class($conn,$_SESSION["user"]);
-                echo("Votre nom d'utilisateurs est".$info["user"]);
-                echo("<br> Votre argent restant est :".$info["general"]);
-                echo("<br> Argent gagné ou perdu aux jeux :");
-                echo("<br> BlackJack :".$info["blackjack"]);
-                echo("<br> Bonnetaeau :".$info["bonneteau"]);
-                echo("<br> Courses à Cheval :".info["chevaux"]);
-                echo("<br> Dés :".info["des"]);
-                echo("<br> Machine à sous :".info["machine_a_sous"]);
-                echo("<br> Roulette :".info["Roulette"]);
+                echo("<p class = 'sign'>Votre nom d'utilisateurs est <strong>".$info["user"]);
+                echo("</strong><br> Votre argent restant est : <strong>".$info["general"]);
+                echo("</strong><br> Argent gagné ou perdu aux jeux :");
+                echo("<br> BlackJack : <strong>".$info["blackjack"]);
+                echo("</strong><br> Bonnetaeau : <strong>".$info["bonneteau"]);
+                echo("</strong><br> Courses à Cheval : <strong>".$info["chevaux"]);
+                echo("</strong><br> Dés : <strong>".$info["des"]);
+                echo("</strong><br> Machine à sous : <strong>".$info["machine_a_sous"]);
+                echo("</strong><br> Roulette : <strong>".$info["roulette"]);
+                echo('</strong></p>');
                 ?>
                 </div>
             </div>
     </div>
+</section>
 </body>
 </html>
 <?php
